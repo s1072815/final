c07f8e0ba19e0bb1bb289c825b1cf69927d6122f
蔡孟翰 <cena2023@gmail.com>
2021-06-24 20:21:46 +0800

    1

---
+++ web410728159/Controllers/ExamController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ExamController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Day(float? name)
+        {
+            var result = "";
+            if (name.HasValue)
+            {
+                result = "星期" + name;
+            }
+            ViewBag.Result = result;
+
+            return View();
+        }
+
+    }
+
+}
